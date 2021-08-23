c32baab390eb689e5854c93099e029256621f051
Ezenwa Chigozie <stonesilver12@gmail.com>
2021-08-23 22:08:33 +0100

    added services and prevented the form from submitting

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -118,7 +118,10 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
-
+var handleSubmit = function handleSubmit(e) {
+  e.preventDefault();
+  alert('Hello!!!!!!!!!!!!');
+};
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62613" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61691" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
